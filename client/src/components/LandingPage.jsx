53493f8ac0302053dad0b4f3a66a2a08057506f5
unknown <cristiangc92@gmail.com>
2023-06-22 15:59:50 -0300

    Agregando estilos a la landing.

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import "./LandingPage.css";
 export default function LandingPage() {
   return (
     <div className="fondoLanding">
-      <h1>Countries SPA</h1>
+      <h1 className="bg-success">Countries SPA</h1>
       <Link to="/home">
         <button type="button" className="btn btn-success btn-lg">
           INGRESAR
